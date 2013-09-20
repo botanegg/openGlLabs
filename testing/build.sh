add6cd7cc91586323b40f61e8bfdcff38afdf5f1
Konstantin Goncharik <botankras@gmail.com>
2013-09-20 19:08:25 +0800

    Added compile flags into CMake

--- testing/build.sh
+++ testing/build.sh
@@ -6,10 +6,7 @@ export CXX=clang++
 mkdir -p build
 cd build
 
-cmake .. \
-    -DCMAKE_CPP_FLAGS="-g -Wall -Wextra" \
-    -DCMAKE_CXX_FLAGS="-g -Wall -Wextra" \
-    -DCMAKE_C_FLAGS="-g -Wall -Wextra"
+cmake ..
 make
 
 mv testing ../testing
